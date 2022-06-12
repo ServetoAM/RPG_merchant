9f0196e0c40421422dbe65ff4064e624dd795219
Serveto Anne-Marie <servetonguyen@outlook.com>
2022-06-12 23:52:19 +0200

    Added properties - miss methods

--- Core/src/main/java/com/rpg/core/Items.java
+++ Core/src/main/java/com/rpg/core/Items.java
@@ -6,4 +6,44 @@ package com.rpg.core;
  */
 public class Items {
     
+    private String nameItem;
+
+    /**
+     * Get the value of nameItem
+     *
+     * @return the value of nameItem
+     */
+    public String getNameItem() {
+        return nameItem;
+    }
+
+    /**
+     * Set the value of nameItem
+     *
+     * @param nameItem new value of nameItem
+     */
+    public void setNameItem(String nameItem) {
+        this.nameItem = nameItem;
+    }
+
+    private String description;
+
+    /**
+     * Get the value of description
+     *
+     * @return the value of description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @param description new value of description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
--- Core/src/main/java/com/rpg/core/SellableItems.java
+++ Core/src/main/java/com/rpg/core/SellableItems.java
@@ -1,9 +1,73 @@
 package com.rpg.core;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Serveto
  */
 public class SellableItems {
     
+    private ArrayList<Items> listMerchandise;
+    
+    private int basePrice;
+
+    /**
+     * Get the value of basePrice
+     *
+     * @return the value of basePrice
+     */
+    public int getBasePrice() {
+        return basePrice;
+    }
+
+    /**
+     * Set the value of basePrice
+     *
+     * @param basePrice new value of basePrice
+     */
+    public void setBasePrice(int basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    private int quantity;
+
+    /**
+     * Get the value of quantity
+     *
+     * @return the value of quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * Set the value of quantity
+     *
+     * @param quantity new value of quantity
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    private String item;
+
+    /**
+     * Get the value of item
+     *
+     * @return the value of item
+     */
+    public String getItem() {
+        return item;
+    }
+
+    /**
+     * Set the value of item
+     *
+     * @param item new value of item
+     */
+    public void setItem(String item) {
+        this.item = item;
+    }
+
 }
